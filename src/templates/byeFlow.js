cad013b0e56b7bebf262450bb4530f2c8857cfe6
roberto.ponce <roberto.ponce@dit.mx>
2025-03-03 12:29:57 -0600

    Cambio URL grupo WA

--- src/templates/byeFlow.js
+++ src/templates/byeFlow.js
@@ -6,7 +6,7 @@ const byeFlow = addKeyword(["salir", "Salir", EVENTS.ACTION]).addAction(
       console.log("Cliente finaliza Chat");
       ctxFn.flowDynamic("Gracias por usar nuestro ChatBot 🤖");
       return ctxFn.endFlow(
-        "Si tienes alguna otra duda, te respondemos personalmente en nuestro grupo de WhatsApp 📲 https://chat.whatsapp.com/JKrVWZqhvyVGVaiQZWuGHK"
+        "Si tienes alguna otra duda, te respondemos personalmente en nuestro grupo de WhatsApp 📲 https://chat.whatsapp.com/I7IQeRQFVh7G0t6WKy6S1y"
       );
     } catch (error) {
       console.log("Error en byeFlow: ", error);
